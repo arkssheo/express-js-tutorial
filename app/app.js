b5dd73c5a6717e42e878e35a8048363d1134a5cf
Jose M <sotero.valdez@gmail.com>
2017-02-19 00:23:31 -0600

    initial commit

--- app/app.js
+++ app/app.js
@@ -3,16 +3,18 @@ var reload = require('reload');
 var app = express();
 var dataFile = require('./data/data.json');
 var io = require('socket.io')();
+var path = require('path');
 
 app.set('port', process.env.PORT || 3000 );
 app.set('appData', dataFile);
 app.set('view engine', 'ejs');
-app.set('views', 'app/views');
+app.set('views', __dirname+'/views');
+//app.set('views', 'app/views');
 
 app.locals.siteTitle = 'Roux Meetups';
 app.locals.allSpeakers = dataFile.speakers;
 
-app.use(express.static('app/public'));
+app.use(express.static(__dirname+'/public'));
 app.use(require('./routes/index'));
 app.use(require('./routes/speakers'));
 app.use(require('./routes/feedback'));
